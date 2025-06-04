64a4b1c2a7e7bb12c80be2eceb337e971f6ec717
Aleksandr Eremeev <aleksandreremeev75@gmail.com>
2025-06-04 15:07:53 +0300

    tt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,18 @@
-#!/bin/sh
-set -e   # немедленно выйдем, если любая команда вернёт ненулевой код
+set -e
 
-# Убедимся, что находимся в нужной директории:
-cd /app/ScreenBook
+cd /app
 
 echo "=== Применяю миграции ==="
-python manage.py migrate --noinput
+python ScreenBook/manage.py migrate --noinput
+
+mkdir -p /app/ScreenBook/static
+chown -R appuser:appuser /app/ScreenBook/static
 
 echo "=== Собираю static файлы ==="
-# В settings.py у вас должен быть STATIC_ROOT = BASE_DIR / 'static'
-python manage.py collectstatic --noinput
+python ScreenBook/manage.py collectstatic --noinput
 
 echo "=== Запускаю Gunicorn ==="
-# Gunicorn будет искать модуль ScreenBook.wsgi (файл /app/ScreenBook/ScreenBook/wsgi.py)
 exec gunicorn ScreenBook.wsgi:application \
-     --bind 0.0.0.0:8000 \
-     --workers 2 \
-     --log-level info
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level info
