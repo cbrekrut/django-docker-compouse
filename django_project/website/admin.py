4867ef2cb7c334a0c63c472d41bac9aed615e22a
Aleksandr Eremeev <aleksandreremeev75@gmail.com>
2025-06-04 12:50:55 +0300

    screenbook

--- django_project/website/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Marker, UserProfile, LeaveAction
-
-admin.site.register(Marker)
-admin.site.register(LeaveAction)  # Новое модель LeaveAction необходим
-admin.site.register(UserProfile)
-# Register your models here.
